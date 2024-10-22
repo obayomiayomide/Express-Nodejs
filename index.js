349b76087ed7ce2cabb1ffe9a544609c830307dd
obayomiayomide <obayomiayomide2020@gmail.com>
2024-10-22 18:17:32 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const port = 3003;
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+const userRoutes = require("./src/routes/auth.route");
+const postRoutes = require("./src/routes/post.route");
+const commentRoutes = require("./src/routes/comment.route");
+
+app.get("/", (req, res) => {
+  res.send("Hello Devs! Whats up");
+});
+
+app.use("/", userRoutes);
+app.use("/post", postRoutes);
+app.use("/comment", commentRoutes);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
