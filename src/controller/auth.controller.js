cab2b30bc030cc425cb4f352cbcb013fe261b043
obayomiayomide <obayomiayomide2020@gmail.com>
2024-10-24 08:08:30 +0100

    first commit

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,12 +1,12 @@
-const jwt = require('jsonwebtoken');
-const prisma = require('../../dbConfig/index');
-const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+const prisma = require("../../dbConfig/index");
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
 const JWT_SECRET = process.env.JWT_SECRET;
 const JWT_EXPIRY = process.env.JWT_EXPIRY;
 
-console.log('secret', JWT_SECRET);
-console.log('expiry', JWT_EXPIRY);
+console.log("secret", JWT_SECRET);
+console.log("expiry", JWT_EXPIRY);
 
 exports.createUser = async (req, res) => {
   try {
@@ -21,11 +21,11 @@ exports.createUser = async (req, res) => {
         password: hashedPassword,
       },
     });
-    console.log('new user is: ', newUser);
+    console.log("new user is: ", newUser);
     res.status(201).json({ user: newUser });
   } catch (error) {
-    console.log('Error is: ', error);
-    res.status(500).json({ error: 'Error creating user' });
+    console.log("Error is: ", error);
+    res.status(500).json({ error: "Error creating user" });
   }
 };
 
@@ -39,7 +39,7 @@ exports.loginUser = async (req, res) => {
     });
 
     if (!user) {
-      return res.status(401).json({ error: 'Invalid Email or password' });
+      return res.status(401).json({ error: "Invalid Email or password" });
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
@@ -49,12 +49,14 @@ exports.loginUser = async (req, res) => {
     });
 
     if (!isPasswordValid) {
-      return res.status(401).json({ error: 'Invalid Email or password' });
+      return res.status(401).json({ error: "Invalid Email or password" });
     }
 
-    res.status(200).json({ message: `Login successful ${user.firstName}`, token });
+    res
+      .status(200)
+      .json({ message: `Login successful ${user.firstName}`, token });
   } catch (error) {
-    console.log('Error is: ', error);
-    res.status(500).json({ error: 'Error logging in user' });
+    console.log("Error is: ", error);
+    res.status(500).json({ error: "Error logging in user" });
   }
 };
